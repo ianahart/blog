f6bc804bc88a5c996fb9b08e83e9bcb66baf9955
Ian Hart <ianhart7790@gmail.com>
2022-02-06 15:45:06 -0500

    feat: styled up the 404 not found page

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -1,8 +1,26 @@
-import { Heading } from '@chakra-ui/react';
+import { Heading, Image, Link } from '@chakra-ui/react';
+import notFoundImage from '../images/not_found.svg';
+import { Link as RouterLink } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <Heading>Page Not Found (404) ='(</Heading>
+    <>
+      <Image src={notFoundImage} alt="three people carry 404 letter blocks" />
+      <Link opacity="0" href="https://storyset.com/online">
+        Online illustrations by Storyset
+      </Link>
+      <Link
+        color="blue.primary"
+        textDecoration="underline"
+        as={RouterLink}
+        to="/"
+        fontSize="20px"
+        fontWeight="bold"
+      >
+        Back to the good stuff
+      </Link>
+    </>
   );
-}
+};
 export default NotFound;
+
