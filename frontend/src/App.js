798ba4e35092e31ee5f7103b3102df0bbc40958e
Ian Hart <ianhart7790@gmail.com>
2022-02-06 15:32:28 -0500

    feat: added a cookie banner

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,15 +11,21 @@ import Login from './pages/Admin/Login';
 import ForgotPassword from './pages/Admin/ForgotPassword';
 import Dashboard from './pages/Admin/Dashboard';
 import NotFound from './pages/NotFound';
+import CookiePolicy from './pages/CookiePolicy.jsx';
+import PrivacyPolicy from './pages/PrivacyPolicy.jsx';
 import AdminPost from './pages/Admin/AdminPost';
 import Post from './pages/Post';
 import Footer from './components/Footer';
 import NavContextProvider from './contexts/NavContext';
 import RequireAuth from './components/Admin/RequireAuth';
+import CookieBanner from './components/Mixed/CookieBanner.jsx';
 import './App.css';
 import WithAxios from './misc/WithAxios';
-
+import { useState } from 'react';
 const App = () => {
+  const [cookieBannerShowing, setCookieBannerShowing] = useState(true);
+  const closeCookieBanner = () => setCookieBannerShowing(false);
+
   return (
     <Router>
       <WithAxios>
@@ -34,6 +40,8 @@ const App = () => {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/posts" element={<Posts />} />
+              <Route path="/cookie-policy" element={<CookiePolicy />} />
+              <Route path="/privacy-policy" element={<PrivacyPolicy />} />
               <Route path="/categories" element={<Categories />} />
               <Route path="/about" element={<About />} />
               <Route path="/admin/login" element={<Login />} />
@@ -90,6 +98,13 @@ const App = () => {
               <Route path="*" element={<NotFound />} />
             </Routes>
           </div>
+
+          {cookieBannerShowing && (
+            <CookieBanner
+              closeCookieBanner={closeCookieBanner}
+              cookieBannerShowing={cookieBannerShowing}
+            />
+          )}
           <Fragment>
             <Footer />
           </Fragment>
---
+++ frontend/src/components/Mixed/CookieBanner.jsx
@@ -0,0 +1,50 @@
+import { Box, Text, Heading, Button, Slide, Link } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
+const CookieBanner = ({ cookieBannerShowing, closeCookieBanner }) => {
+  return (
+    <Slide direction="bottom" in={cookieBannerShowing} style={{ zIndex: 10 }}>
+      <Box
+        backgroundColor="light.primary"
+        boxShadow="lg"
+        border=" 1px solid dark.secondary"
+        borderRadius={8}
+        width="80%"
+        margin="0 auto"
+        p="1rem"
+      >
+        <Heading as="h4">This website uses cookies</Heading>
+        <Text lineHeight="1.6">
+          We use cookies to provide you with the best possible experience. They also allow us to
+          analyze user behavior in order to constantly improve the website for you.
+        </Text>
+        <Box display="flex" width="400px">
+          <Link
+            display="inline-block"
+            mx={3}
+            color="blue.primary"
+            textDecoration="underline"
+            as={RouterLink}
+            to="/privacy-policy"
+          >
+            Privacy Policy
+          </Link>
+          <Link color="blue.primary" textDecoration="underline" as={RouterLink} to="/cookie-policy">
+            Cookie Policy
+          </Link>
+        </Box>
+        <Box display="flex" mt={5} mb={1} justifyContent="flex-end">
+          <Button
+            backgroundColor="green.primary"
+            display="inline-block"
+            width="120px"
+            mx={3}
+            onClick={() => closeCookieBanner()}
+          >
+            Ok
+          </Button>
+        </Box>
+      </Box>
+    </Slide>
+  );
+};
+export default CookieBanner;
