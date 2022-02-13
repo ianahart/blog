c81791b920474bbe50655490f829531f1b6d4098
Ian Hart <ianhart7790@gmail.com>
2022-02-13 13:26:42 -0500

    fix: trying to deploy to heroku

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,10 +1,19 @@
 
-from asyncio import futures
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
+import os
 
-engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, future=True)
+load_dotenv()
+url = None
+if os.getenv('ENV') == 'production':
+    url = os.getenv('DATABASE_URL')
+    url = url.replace('postgres://', 'postgresql://')
+else:
+    url = settings.SQLALCHEMY_DATABASE_URI
+
+engine = create_engine(url, future=True)
 
 SessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=engine, future=True)
